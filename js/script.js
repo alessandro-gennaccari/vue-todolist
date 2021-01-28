2086dc9792e13de2aa4afc3c4d96f22155804ba2
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-28 14:03:59 +0100

    added-remove_todo-save_todo_removed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 var todoList = new Vue({
-    
+    el: '#todo',
+    data: {
+        todos: [
+            'cane',
+            'prova2',
+            'prova3?'
+        ],
+        todosDeleted: []
+    },
+    methods: {
+        deleteTodo(index){
+            this.todosDeleted.push(this.todos[index])
+            this.todos.splice(index,1);
+        }
+    }
 });
